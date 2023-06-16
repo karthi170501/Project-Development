17cc60572952d3528f45520bbf9bb0fac2db9f47
DarshiniRavi <97232190+DarshiniRavi@users.noreply.github.com>
2023-06-16 11:52:49 +0530

    Update Sidenav.js

--- working/navigation/Sidenav.js
+++ working/navigation/Sidenav.js
@@ -1,1 +1,48 @@
 
+import React from "react";
+import "./Sidenav.css";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import ChatIcon from "@mui/icons-material/Chat";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import PersonIcon from '@mui/icons-material/Person';
+
+function Sidenav() {
+                 
+  return (
+    <div className="sidenav">
+      
+
+      <div className="sidenav__buttons">
+        <button className="sidenav__button">
+          <HomeIcon/>
+          <span>Home</span>
+        </button>
+        <button className="sidenav__button">
+          <SearchIcon />
+          <span>Search</span>
+        </button>
+        
+        <button className="sidenav__button">
+          <ChatIcon />
+          <span>Messages</span>
+        </button>
+        <button className="sidenav__button">
+          <FavoriteBorderIcon />
+          <span>Notifications</span>
+        </button>
+        <button className="sidenav__button">
+          <PersonIcon />
+          <span>Profile</span>
+        </button>
+        <button className="sidenav__button">
+          <AddCircleOutlineIcon />
+          <span>Create</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Sidenav;
